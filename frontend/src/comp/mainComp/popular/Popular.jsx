6d81c30c7d3594a665903a272f8406494c2246bb
Najib Muhammadi <najibmuhammadi003@gmail.com>
2024-06-16 13:14:20 +0200

    stylat lite hur jag vill produkter

--- frontend/src/comp/mainComp/popular/Popular.jsx
+++ frontend/src/comp/mainComp/popular/Popular.jsx
@@ -6,35 +6,41 @@ import swiperImgTwo from '../../../assets/images/p19-176.png';
 function Popular() {
     return (
         <div className='popular__container'>
-            <h1 className='popular__title'>Populära Produkter</h1>
-            <div className='popular__swiper'>
-                <div className='swiper__slide'>
-                    <img className='swiper__img' src={swiperImgOne} alt="placeholder bild" />
-                    <h2 className='swiper__title'>Produkt 1</h2>
-                    <p className='swiper__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h1 className='popular__title'>Trending This Week</h1>
+            <div className='popular__product-container'>
+                <div className='product__bigCard'></div>
+                <div className='product__bigCard-desc'>
+                    <h2 className='product__bigCard-title'>Nike Air Force 1 Flyknit 2.0</h2>
+                    <p className='product__bigCard-text'>Shoes</p>
+                    <p className='product__bigCard-price'>1 299 kr</p>
                 </div>
-                <div className='swiper__slide'>
-                    <img className='swiper__img' src={swiperImgTwo} alt="placeholder bild" />
-                    <h2 className='swiper__title'>Produkt 2</h2>
-                    <p className='swiper__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className='product__smallCard'>
+                    <img className='product__smallCard-img' src={swiperImgOne} alt="placeholder bild" />
+                    <div className='product__smallCard-desc'>
+                        <h2 className='product__smallCard-title'>Produkt 3</h2>
+                        <p className='product__smallCard-text'>Shoes</p>
+                        <p className='product__smallCard-price'>1 299 kr</p>
+                    </div>
                 </div>
-                <div className='swiper__slide'>
-                    <img className='swiper__img' src={swiperImgOne} alt="placeholder bild" />
-                    <h2 className='swiper__title'>Produkt 3</h2>
-                    <p className='swiper__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className='product__smallCard'>
+                    <img className='product__smallCard-img' src={swiperImgOne} alt="placeholder bild" />
+                    <div className='product__smallCard-desc'>
+                        <h2 className='product__smallCard-title'>Produkt 3</h2>
+                        <p className='product__smallCard-text'>Lorem</p>
+                        <p className='product__smallCard-price'>1 299 kr</p>
+                    </div>
                 </div>
-                <div className='swiper__slide'>
-                    <img className='swiper__img' src={swiperImgTwo} alt="placeholder bild" />
-                    <h2 className='swiper__title'>Produkt 4</h2>
-                    <p className='swiper__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className='product__smallCard'>
+                    <img className='product__smallCard-img' src={swiperImgOne} alt="placeholder bild" />
+                    <div className='product__smallCard-desc'>
+                        <h2 className='product__smallCard-title'>Produkt 3</h2>
+                        <p className='product__smallCard-text'>Lorem ipsum dolor</p>
+                        <p className='product__smallCard-price'>1 299 kr</p>
+                    </div>
                 </div>
-                <div className='swiper__slide'>
-                    <img className='swiper__img' src={swiperImgOne} alt="placeholder bild" />
-                    <h2 className='swiper__title'>Produkt 5</h2>
-                    <p className='swiper__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                
+
             </div>
+
         </div>
     )
 }
