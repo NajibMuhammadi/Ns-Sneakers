46126e8c5f5b569f07a0d99d4c3be1b85499c703
Najib Muhammadi <najibmuhammadi003@gmail.com>
2024-07-03 22:44:39 +0200

    användaren med giltig token får lägga till profile-img med hjälp av multer

--- backend/routes/userLoginRoute.js
+++ backend/routes/userLoginRoute.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
 import UserController from "../controllers/userController.js";
+import upload from '../multer/upload.js';
+import {userDb} from '../models/userModels.js';
 
 const router = Router();
 const controller = new UserController;
@@ -25,5 +27,31 @@ router.get('/allusers', controller.checkAuthUser, controller.checkIsAdmin, contr
  
 router.put('/insertadmin/:id', controller.checkAuthUser, controller.checkIsAdmin, controller.insertAdminTrue);
 router.put('/insertadminfalse/:id', controller.checkAuthUser, controller.checkIsAdmin, controller.isertAdminFalse);
+router.post('/upload', controller.checkAuthUser, upload.single('image'), async(req, res) => {
+    const userId = req.user.userId;
+    const user = await userDb.findOne({ userId });
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        });
+    }
+
+    if(!req.file) {
+        return res.status(400).json({
+            success: false,
+            message: 'No image uploaded'
+        });
+    }
+
+    userDb.update({ userId }, { $set: { image: req.file.filename } });
+
+    res.status(200).json({
+        filename: req.file.filename,
+        success: true,
+        message: 'Image uploaded successfully'
+    });
+});
 
 export default router
